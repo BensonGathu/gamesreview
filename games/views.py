e6b49dd9c796b61a08b7e8fdec35514c6f3d1eea
Benson <bensongathu23@gmail.com>
2021-07-27 16:08:25 +0300

    Reviews,profile and single game views added

--- games/views.py
+++ games/views.py
@@ -5,3 +5,36 @@ from . models import Profile,Game,Review
 def index(request):
     games = Game.objects.order_by("-id")
     return render(request,'index.html',{'games':games})
+
+
+def reviews(request,id):
+    all_reviews = Review.get_reviews(id)
+    game = get_object_or_404(Game,pk=id)
+
+    form = NewReviewForm()
+    if request.method == 'POST':
+        form = NewReviewForm(request.POST)
+        if form.is_valid()
+            review = form.save(commit=False)
+            review.game = game
+            review.user = request.user
+            review.save()
+        return HttpResponseRedirect(request.path_info)
+    else:
+        form = NewReviewForm()
+    return render(request,'reviews.html',{"all_reviews":all_reviews,"form":form})
+
+
+def singlegame(request,id):
+    game = Game.objects.get(id=id)
+
+    return render(request,'game.html',{"game":game})
+
+def profile(request):
+    pass
+
+def upload_game(request):
+    pass
+
+def search_game(request):
+    pass
